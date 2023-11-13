5bf8bc43a31b9d4a129d8bfb418bbe3e0e30df3f
Toma Kazanskaya <tkazanskaya888@gmail.com>
2023-11-14 02:05:34 +0300

    добавляет счетчик комментариев

--- js/picture.js
+++ js/picture.js
@@ -1,10 +1,60 @@
-import { renderComment, showCommentCount } from './comment.js';
+const COMMENTS_AMOUNT_SHOW = 5;
+
+let commentsAmountShown = 0;
+
+let comments = [];
 
 const bigPhoto = document.querySelector('.big-picture');
 const bodyElement = document.querySelector('body');
 const buttonBigPhotoClose = bigPhoto.querySelector('.big-picture__cancel');
 
+const totalCommentCount = document.querySelector('.social__comment-total-count');
+const commentContainer = document.querySelector('.social__comments');
+const commentCount = document.querySelector('.social__comment-shown-count');
+const commentLoader = document.querySelector('.social__comments-loader');
+
+const commentElement = document.querySelector('#comment').content.querySelector('.social__comment');
+
+const createComment = ({avatar, name, message}) => {
+  const comment = commentElement.cloneNode(true);
+
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
+  comment.querySelector('.social__text').textContent = message;
+
+  return comment;
+};
+
+const renderComments = () => {
+
+  commentsAmountShown += COMMENTS_AMOUNT_SHOW;
+
+  if (commentsAmountShown >= comments.length) {
+    commentLoader.classList.add('hidden');
+    commentsAmountShown = comments.length;
+
+  } else {
+    commentLoader.classList.remove('hidden');
+  };
+
+  const fragmentContainerComment = document.createDocumentFragment();
+  for (let i = 0; i < commentsAmountShown; i++) {
+    const commentNewElement = createComment(comments[i]);
+    fragmentContainerComment.appendChild(commentNewElement);
+  }
+
+  commentContainer.innerHTML = '';
+  commentContainer.appendChild(fragmentContainerComment);
+
+  commentCount.textContent = commentsAmountShown;
+  totalCommentCount.textContent = comments.length;
+};
+
+const onCommentsLoader = () => renderComments();
+
+
 const hidePhoto = () => {
+  commentsAmountShown = 0;
   bigPhoto.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
   document.removeEventListener('keydown', isEscapeKey);
@@ -32,9 +82,10 @@ const showPicture = (pictureData) => {
   bigPhoto.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
 
-  renderComment(pictureData.comments);
-
-  showCommentCount();
+  comments = pictureData.comments;
+  if (comments.length > 0) {
+    renderComments();
+  }
 
   document.addEventListener('keydown', isEscapeKey);
 
@@ -42,5 +93,6 @@ const showPicture = (pictureData) => {
 };
 
 buttonBigPhotoClose.addEventListener('click', onCloseButtonBigPhoto);
+commentLoader.addEventListener('click', onCommentsLoader);
 
 export { showPicture };
